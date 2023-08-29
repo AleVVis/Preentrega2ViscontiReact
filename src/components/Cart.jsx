d4d9c3180b50f29f402d189306e1c3c02d7c2502
AleVVis <alejandra.visconti@hotmail.com>
2023-08-29 20:30:21 -0300

    corrección botones del Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,17 +1,14 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { CartContext } from '../context/CartContext';
 import { Box, Text, Button, Stack, VStack } from '@chakra-ui/react';
 
 const Cart = () => {
-  const { cart, setCart } = useContext(CartContext);
+  const { cart, removeFromCart, clearCart } = useContext(CartContext);
 
-  const removeProduct = (productId) => {
-    const updatedCart = cart.filter(product => product.id !== productId);
-    setCart(updatedCart);
-  };
-
-  const clearCart = () => {
-    setCart([]);
+  const handleRemoveProduct = (productId) => {
+    console.log('Removing product with ID:', productId);
+    removeFromCart(productId);
   };
 
   const calculateTotalPrice = () => {
@@ -21,7 +18,7 @@ const Cart = () => {
   const handleBuy = () => {
     // Implement logic for handling the purchase
     alert('Compra realizada');
-    setCart([]);
+    clearCart();
   };
 
   return (
@@ -29,35 +26,44 @@ const Cart = () => {
       <Text fontSize="xl" fontWeight="bold" mb="4">
         Carrito de Compras
       </Text>
-      <VStack spacing="4" align="stretch">
-        {cart.map((product) => (
-          <Box key={product.id} borderWidth="1px" p="4" bg="white">
-            <Text fontSize="lg" fontWeight="bold">{product.nombre}</Text>
-            <Text>Precio unitario: {product.precio}</Text>
-            <Text>Cantidad: {product.cantidad}</Text>
-            <Text>Total por producto: {product.precio * product.cantidad}</Text>
-            <Button colorScheme="red" onClick={() => removeProduct(product.id)}>
-              Cancelar Producto
-            </Button>
-          </Box>
-        ))}
-      </VStack>
+      {cart.length > 0 ? (
+        <VStack spacing="4" align="stretch">
+          {cart.map((product) => (
+            <Box key={product.id} borderWidth="1px" p="4" bg="white">
+              <Text fontSize="lg" fontWeight="bold">{product.nombre}</Text>
+              <Text>ID: {product.id}</Text>
+              <Text>Precio unitario: {product.precio}</Text>
+              <Text>Cantidad: {product.cantidad}</Text>
+              <Text>Total por producto: {product.precio * product.cantidad}</Text>
+              <Button colorScheme="red" onClick={() => handleRemoveProduct(product.id)}>
+                Cancelar Producto
+              </Button>
+            </Box>
+          ))}
+        </VStack>
+      ) : (
+        <VStack spacing="4" align="stretch">
+          <Text>No hay productos en el carrito</Text>
+          <Link to="/category/all">
+            <Button colorScheme="teal">Ir al catálogo</Button>
+          </Link>
+        </VStack>
+      )}
+      {cart.length > 0 && (
+        <Stack mt="4" spacing="2" direction="row" align="center">
+          <Button colorScheme="red" onClick={clearCart}>
+            Borrar Carrito
+          </Button>
+          <Button colorScheme="teal" onClick={handleBuy}>
+            Comprar
+          </Button>
+        </Stack>
+      )}
       <Box mt="4">
         <Text fontWeight="bold">Total: {calculateTotalPrice()}</Text>
       </Box>
-      <Stack mt="4" spacing="2" direction="row" align="center">
-        <Button colorScheme="red" onClick={clearCart}>
-          Borrar Carrito
-        </Button>
-        <Button colorScheme="teal" onClick={handleBuy}>
-          Comprar
-        </Button>
-      </Stack>
     </Box>
   );
 };
 
 export default Cart;
-
-
-
